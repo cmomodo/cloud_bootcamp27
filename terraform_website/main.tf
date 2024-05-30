3cb70c619a5ee176433ad974dd0fb26a8f715cbb
cmomodo <ceesay.ml@outlook.com>
2024-05-30 17:53:15 +0100

    This is the s3 bucket that host the website with its bucket policy

--- terraform_website/main.tf
+++ terraform_website/main.tf
@@ -7,12 +7,44 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+# Create an S3 bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "www.cea27.com"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+# Set public access block configuration
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls   = false
+  block_public_policy = false
+}
+
+# Set bucket policy to make it publicly accessible
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Statement1",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::www.cea27.com/*"
+        }
+    ]
+}
+POLICY
 }
