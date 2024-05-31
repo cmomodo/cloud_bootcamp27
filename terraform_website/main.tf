d9fad8407571a9cf86d54adf056beff406dfec8d
cmomodo <ceesay.ml@outlook.com>
2024-05-31 09:03:16 +0100

    chore: Configure website hosting for S3 bucket and set public access block configuration

--- terraform_website/main.tf
+++ terraform_website/main.tf
@@ -21,6 +21,29 @@ resource "aws_s3_bucket" "example" {
   }
 }
 
+# Configure website hosting for the S3 bucket
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  index_document {
+    suffix = "index.html"
+  } 
+  error_document {
+    key = "error.html"
+  
+  }
+
+  routing_rule {
+    condition {
+      key_prefix_equals = "docs/"
+    }
+    redirect {
+      replace_key_prefix_with = "documents/"
+    }
+  }
+  
+}
+
 # Set public access block configuration
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
